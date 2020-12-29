dff0d7d910bbad288e6a5782c37bd195607bf800
Eneki <joanreneki@gmail.com>
2020-12-29 15:10:21 +0300

    Add graphql dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operator';
+
+const GET_QUOTES = gql;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor() {
+
+  }
+  ngOnInit() {}
 }
